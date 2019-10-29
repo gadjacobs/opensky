e2c1f87dc81a528745b5c73a0c97a443a14202b6
Gad Jacobs <gadexofficial@gmail.com>
2019-10-29 20:07:09 +0100

    Homepage

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+const Home = () => {
+    return (
+        <div>
+            <Button>Hello</Button>
+        </div>
+    );
+}
+
+export default Home;
