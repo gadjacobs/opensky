c43e0c0464578172713f322340e62b80bfa0d05e
Gad Jacobs <gadexofficial@gmail.com>
2019-10-30 12:23:34 +0100

    Departure and arrival times

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -34,9 +34,30 @@ export default function SimpleModal({ icao }) {
   const [open, setOpen] = React.useState(false);
   const [flights, setFlights] = React.useState([]);
 
+  const startTime = parseInt(
+    (new Date("2019.10.29").getTime() / 1000).toFixed(0)
+  );
+
+  const endTime = parseInt(
+    (new Date("2019.10.30").getTime() / 1000).toFixed(0)
+  );
+
+  function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+  }
+
   async function fetchFlights(icao, start, end) {
     const res = await fetch(
-      `https://opensky-network.org/api/flights/aircraft?icao24=${icao}&begin=1517184000&end=1517270400`
+      `https://opensky-network.org/api/flights/aircraft?icao24=${icao}&begin=${start}&end=${end}`
     );
     res
       .json()
@@ -45,7 +66,7 @@ export default function SimpleModal({ icao }) {
   }
 
   const handleOpen = () => {
-    fetchFlights(icao);
+    fetchFlights(icao, startTime, endTime);
     setOpen(true);
   };
 
@@ -66,10 +87,15 @@ export default function SimpleModal({ icao }) {
       >
         <div style={modalStyle} className={classes.paper}>
           <h2 id="simple-modal-title">{`Aircraft: ${icao}`}</h2>
-          <h4>Flights Today:</h4>
+          <h4>Flights in the Last 24 hours:</h4>
           {flights.map((flight, i) => (
             <p id="simple-modal-description">
-              {flight !== undefined || [] ? flight.callsign : flight.length === 0 ? "There are no flights within this period. Please check again later" : "Loading Flights"}
+              {
+                  flight !== undefined || []
+                ? `Call Sign: ${flight.callsign}, Departure: ${timeConverter(flight.firstSeen)}, Arrival: ${timeConverter(flight.lastSeen)}`
+                : flight.length === 0
+                ? "There are no flights within this period. Please check again later"
+                : "Loading Flights"}
             </p>
           ))}
         </div>
